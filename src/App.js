9cc9a7a88105b3ad04ca232c9eb748aa542c1e1e
Plasma Intec <plasmaintec@i.local>
2019-12-30 22:44:37 -0800

    Run eslint

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import Layout from "./Layout.js";
 
 class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-		<Layout />
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div className="App">
+                <h1> Hello, World! </h1>
+                <Layout />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Layout.js
+++ src/Layout.js
@@ -3,40 +3,40 @@ import ReactDOM from "react-dom";
 import Chart from "chart.js";
 
 class Layout extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
   
-  componentDidMount() {
-    var el = ReactDOM.findDOMNode(this);
+    componentDidMount() {
+        var el = ReactDOM.findDOMNode(this);
 
-    var myChart = new Chart(el, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow"],
-        datasets: [
-          {
-            label: "# of Likes",
-            data: [12, 19, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)"
-            ]
-          }
-        ]
-      }
-    });
-  }
+        var myChart = new Chart(el, {
+            type: "bar",
+            data: {
+                labels: ["Red", "Blue", "Yellow"],
+                datasets: [
+                    {
+                        label: "# of Likes",
+                        data: [12, 19, 3],
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.2)",
+                            "rgba(54, 162, 235, 0.2)",
+                            "rgba(255, 206, 86, 0.2)"
+                        ]
+                    }
+                ]
+            }
+        });
+    }
 
-  render() {
-    return (
-      <canvas
-        id="chart"
-        style={{ width: 800, height: 300 }}
-      />
-    );
-  }
+    render() {
+        return (
+            <canvas
+                id="chart"
+                style={{ width: 800, height: 300 }}
+            />
+        );
+    }
 }
 
 export default Layout;
